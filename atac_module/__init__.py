3348700324359346872063a23205542795ed03f0
Benjamin James <benjames@mit.edu>
2022-07-20 21:06:28 -0400

    added neighbors_dense to default

--- atac_module/__init__.py
+++ atac_module/__init__.py
@@ -3,3 +3,4 @@ from .extraction import extract_rep
 from .spline import fit_splines
 from .neighbors import neighbors
 from .cluster import filter_var, leiden, infomap
+from .dense import neighbors_dense
