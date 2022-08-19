36594e9aaa4907520e0c36329c8a3d2a700e4410
Benjamin James <benjames@mit.edu>
2022-08-19 00:18:07 -0400

    removed spline name from interface since not using splines

--- epiclust/__init__.py
+++ epiclust/__init__.py
@@ -1,6 +1,6 @@
 #from .perbin import calc_perbin_stats
 from .extraction import extract_rep
-from .spline import fit_splines
+from .fit import fit
 from .neighbors import neighbors
 from .cluster import filter_var, leiden, infomap
 from .gene_estimation import estimate_genes_linking
