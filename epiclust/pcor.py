bc664cc87162fbcf103894451651044ae935cb96
Benjamin James <benjames@mit.edu>
2022-08-19 21:46:32 -0400

    Modified pcor to re-add remove covariates

--- epiclust/pcor.py
+++ epiclust/pcor.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 import scipy.sparse
-from .utils import nearPD, isPD
+
 def adjust_partial_cor(mat, L, R, inv):
         """formula: sub = L INV R^T
         f"""
@@ -14,25 +14,6 @@ def adjust_partial_cor(mat, L, R, inv):
         denom_square = np.clip(denom_square, a_min=1e-20, a_max=np.inf)
         one_over_det = denom_square - np.multiply(mat - sub, mat - sub)
         return np.sign(one_over_det) * (mat - sub)/np.sqrt(denom_square)
-
-def full_partial_correct(zmat, row, col, epsilon=1e-50):
-        cor = np.tanh(zmat)
-        for i in range(5):
-                if not isPD(cor):
-                        cor = nearPD(cor + epsilon)
-                else:
-                        break
-        try:
-                icor = np.linalg.inv(cor)
-        except:
-                val, vec = np.linalg.eigh(cor)
-                val[np.abs(val) < 1e-10] = 1e-10
-                icor = vec @ np.diag(1/val) @ vec.T
-                pass
-        D = np.diag(icor) + 0
-        D[np.abs(D) < epsilon] = epsilon
-        neg_out = icor * np.outer(D, D)**(-0.5)
-        return np.arctanh(np.clip(-neg_out, -1+1e-16, 1-1e-16))
 
 class PartialCor:
         def __init__(self, feat_cor_mat_L, self_cor_mat, feat_cor_mat_R, batch_size=5000):
@@ -48,8 +29,9 @@ class PartialCor:
                         self_cor_mat = np.array(self_cor_mat).reshape(1, 1)
                 assert self.lfcor.shape[1] == self_cor_mat.shape[0]
                 assert self.rfcor.shape[0] == self_cor_mat.shape[1]
-                if not isPD(self_cor_mat):
-                        self_cor_mat = nearPD(self_cor_mat)
+                ### TODO: check nearPD
+                # if not isPD(self_cor_mat):
+                #         self_cor_mat = nearPD(self_cor_mat)
                 self.ainv = np.linalg.inv(self_cor_mat)
                 self.batch_size = batch_size
         def __call__(self, mat, row=None, col=None, eps=1e-16):
@@ -75,3 +57,29 @@ class PartialCor:
                                                                                    inv=self.ainv)
                 out = np.arctanh(np.clip(out, a_min=-1+eps, a_max=1-eps))
                 return (out + mat)/2 ## average
+
+def adjust_covariates(adata, covariates, min_variance=1e-20, batch_size=10000):
+        import numpy as np
+        import pandas as pd
+        from sklearn.decomposition import PCA
+        br = pd.get_dummies(adata.obs[covariates]).values
+        br = br[:, np.std(br, axis=0) > 0] ### remove zero variance cols e.g. pd.Categorical not present in data
+        ### first compute full rank PCA:
+        pca = PCA(n_components=br.shape[1]).fit(br)
+        n_comp = np.sum(pca.explained_variance_ratio_ > min_variance)
+        ### then trim to maximize variance
+        br = PCA(n_components=n_comp).fit_transform(br)### zero centered
+        br = br / np.linalg.norm(br, axis=0, ord=2)[None, :]
+        RR = br.T.dot(br) ### corr coef
+        PR = np.zeros((adata.shape[1], br.shape[1]))
+        br = br - br.mean(0)[:, None]
+        for left in np.arange(0, adata.shape[1], batch_size):
+                right = min(left + batch_size, adata.shape[1])
+                if scipy.sparse.issparse(adata.X):
+                        X = np.asarray(adata.X[:, left:right].todense(), dtype=np.float64)
+                else:
+                        X = np.asarray(adata.X[:, left:right], dtype=np.float64)
+                X = X - X.mean(0)[None, :]
+                X = X / np.linalg.norm(X, ord=2, axis=0)[None, :].clip(1e-50, np.inf)
+                PR[left:right, :] = X.T.dot(br)
+        return PartialCor(PR, RR, PR.T, batch_size=batch_size)
