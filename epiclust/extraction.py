c95e1f3a076a6d9510cb217d73f2dd4f4418468b
Benjamin James <benjames@mit.edu>
2022-10-19 10:19:55 -0400

    extraction rewrite to better model high powers

--- epiclust/extraction.py
+++ epiclust/extraction.py
@@ -1,32 +1,38 @@
 
-import numpy as np
-
-
-def extract_pca(adata, n_pcs=None):
+def extract_pca(adata, n_pcs=None, extract_u=True):
     """we know that the S component is always positive
     so it can be recontructed from s^2/(DoF)"""
+    import numpy as np
     Us = adata.obsm["X_pca"]
-    s = adata.uns["pca"]["variance"]
+    s = adata.uns["pca"]["variance"].astype(np.float64)
     s = np.sqrt(s * (adata.shape[0] - 1))
-    U = Us @ np.diag(1 / s)
+    if extract_u:
+        U = Us @ np.diag(1 / s)
     VT = adata.varm["PCs"].T
     if n_pcs is not None and n_pcs <= len(s):
-        U = U[:, range(n_pcs)]
+        if extract_u:
+            U = U[:, range(n_pcs)]
         s = s[range(n_pcs)]
         VT = VT[range(n_pcs), :]
+    if not extract_u:
+        U = None
     return U, s, VT
 
 
 def extract_rep(adata, power=0.0, margin="log1p_total_counts",
                 key_added="epiclust", n_pcs=None, zero_center=True):
+    import numpy as np
     if margin not in adata.var.columns:
         print("Margin", margin, "not in adata.var")
         return -1
     if "PCs" not in adata.varm or "X_pca" not in adata.obsm or "pca" not in adata.uns:
         print("Must run sc.pp.pca first")
         return -1
-    U, s, VT = extract_pca(adata, n_pcs=n_pcs)
-    X_adj = VT.T.astype(np.float64) @ np.diag(s**power)
+    _, s, VT = extract_pca(adata, n_pcs=n_pcs, extract_u=False)
+    s = s.astype(np.float64) ** power
+    s = s / np.linalg.norm(s, ord=2).clip(1e-100, np.inf)
+    X_adj = VT.T.astype(np.float64) @ np.diag(s)
+    del VT
     if zero_center:
         X_adj = X_adj - X_adj.mean(1)[:, None]
     norm = np.linalg.norm(X_adj, axis=1, ord=2)[:, None]
